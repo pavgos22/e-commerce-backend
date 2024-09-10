969b3307a655114b6f02ec01d8cffed4e4fae7da
Paweł Gościak DEV <pawgosciak@gmail.com>
2024-09-10 17:05:21 +0200

    Route Validator

---
+++ gateway/src/main/java/com/projects/gateway/filter/RouteValidator.java
@@ -0,0 +1,24 @@
+package com.projects.gateway.filter;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/register",
+            "/auth/login",
+            "/auth/validate"
+    );
+
+    public Predicate<ServerHttpRequest> isSecure =
+            request-> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri->request.getURI()
+                            .getPath()
+                            .contains(uri));
+}
+
